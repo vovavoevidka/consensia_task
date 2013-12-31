e2c2fbad26d4b085ce8883117eff503c574ee13f
vova <Voevidka.vova.v@gmail.com>
2013-12-31 15:09:56 +0200

    some fixes;

--- js/app.js
+++ js/app.js
@@ -1,6 +1,20 @@
 ﻿var app = angular.module('app', ['ngResource'])
     .config(['$httpProvider', function ($httpProvider) {
-        $httpProvider.defaults.useXDomain = true;
-        delete $httpProvider.defaults.headers.common['X-Requested-With'];
+    	//delete $httpProvider.defaults.headers.common["X-Requested-With"];
     }])
-    .controller('productController', productController);
+	//.config(function($httpProvider) {
+	//	$httpProvider.responseInterceptors.push(function ($q) {
+	//		return function (promise) {
+	//			return promise.then(function (response) {
+	//				return response;
+	//			}, 
+	//			function (response) {
+	//				if (response.status == 401) {
+	//					window.location.href = '/Account/Login?redirectUri=' + encodeURIComponent(window.location.href);
+	//					return response;
+	//				}
+	//				return response;
+	//			});
+	//		};
+	//	}) })
+	.controller('productController', productController);
--- js/controllers/productController.js
+++ js/controllers/productController.js
@@ -1,17 +1,52 @@
-﻿var productController = function ($scope, $resource, $http) {
-    $http.defaults.useXDomain = true;
-    $http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
-    /*http://api.saxo.com/v1/products/18267552.xml?key=563c52b3177047978c0bdfbfd776ebd3*/
-    //var Book = $resource('http://api.saxo.com/v1/products/18267552.xml?key=563c52b3177047978c0bdfbfd776ebd3');
-    //var book = Book.get({},
-    //    function (data) {
-    //        console.log(book);
-    //    },
-    //    function (error) {
-    //    });
-    $http({ method: 'GET', url: 'http://api.saxo.com/v1/products/18267552.json?key=563c52b3177047978c0bdfbfd776ebd3' })
-    .success(function(data, status){
-        console.log(data);
-    })
-    .error(function(data, status){});
+﻿
+window.jsonp_callback = function (data) {
+	console.log(data.found);
+}
+
+var productController = function ($scope, $resource, $http) {
+	//$http.defaults.useXDomain = true;
+	//$http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
+
+	$scope.url = "http://api.saxo.com/v1/products/18267552.json?callback=jsonp_callback&key=563c52b3177047978c0bdfbfd776ebd3";
+	//$scope.url = "http://public-api.wordpress.com/rest/v1/sites/wtmpeachtest.wordpress.com/posts?callback=jsonp_callback";
+	//var Book = $resource($scope.url);
+	//var book = Book.get({},
+	//    function (data) {
+	//    	console.log(data);
+	//    },
+	//    function (error) {
+	//    });
+
+
+	//$http({ method: 'GET', url: 'http://api.saxo.com/v1/products/18267552.json?key=563c52b3177047978c0bdfbfd776ebd3' })
+	//.success(function(data, status){
+	//	console.log(data);
+	//})
+	//.error(function (data, status) {
+	//	console.error(data);
+	//});
+
+	//delete $http.defaults.headers.common['X-Requested-With'];
+
+	//$scope.data = "unknown";
+	//$http.get($scope.url, { headers: { 'Access-Control-Allow-Origin': "*", 'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE', 'Access-Control-Allow-Headers': 'Content-Type' } }).success(function (data) {
+	//	$scope.data = data;
+	//});
+
+	//$http.jsonp($scope.url).success(function (data) {
+	//	$scope.data = data;
+	//});
+
+	$http.jsonp($scope.url);
+
+	//$.ajax({
+	//	url: 'http://api.saxo.com/v1/products/18267552.json?key=563c52b3177047978c0bdfbfd776ebd3',
+	//	type: "GET",
+	//	async: false,
+	//	dataType: 'text',
+	//	crossDomain: true,
+	//	success: function(data, status){
+	//		console.log(data);
+	//	},
+	//});
 }

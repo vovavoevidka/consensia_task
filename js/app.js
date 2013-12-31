5ee7ea70130b32d44c04cd72e04c5c6215ec1a79
vova voevidka <voevidka.vova.v@gmail.com>
2013-12-31 10:55:46 +0200

    initial

---
+++ js/app.js
@@ -0,0 +1,6 @@
+﻿var app = angular.module('app', ['ngResource'])
+    .config(['$httpProvider', function ($httpProvider) {
+        $httpProvider.defaults.useXDomain = true;
+        delete $httpProvider.defaults.headers.common['X-Requested-With'];
+    }])
+    .controller('productController', productController);
---
+++ js/controllers/productController.js
@@ -0,0 +1,17 @@
+﻿var productController = function ($scope, $resource, $http) {
+    $http.defaults.useXDomain = true;
+    $http.defaults.headers.post["Content-Type"] = "application/x-www-form-urlencoded";
+    /*http://api.saxo.com/v1/products/18267552.xml?key=563c52b3177047978c0bdfbfd776ebd3*/
+    //var Book = $resource('http://api.saxo.com/v1/products/18267552.xml?key=563c52b3177047978c0bdfbfd776ebd3');
+    //var book = Book.get({},
+    //    function (data) {
+    //        console.log(book);
+    //    },
+    //    function (error) {
+    //    });
+    $http({ method: 'GET', url: 'http://api.saxo.com/v1/products/18267552.json?key=563c52b3177047978c0bdfbfd776ebd3' })
+    .success(function(data, status){
+        console.log(data);
+    })
+    .error(function(data, status){});
+}
